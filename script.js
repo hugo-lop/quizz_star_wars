176d8a3676aa751463340a0913d9cfa156772dc1
hugo-lop <hugo.menage.perso@gmail.com>
2026-02-12 17:39:57 +0100

    2d commit

---
+++ script.js
@@ -0,0 +1,291 @@
+    /* =========================
+           QUESTIONS FR
+        ========================== */
+        const questionsFR = [
+            {
+                question: "Quel est le vrai nom de Dark Vador ?",
+                choix: ["Luke Skywalker", "Anakin Skywalker", "Obi-Wan Kenobi", "Han Solo"],
+                bonne: 1
+            },
+            {
+                question: "Quelle planète est détruite par l'Étoile de la Mort ?",
+                choix: ["Hoth", "Alderaan", "Tatooine", "Naboo"],
+                bonne: 1
+            },
+            {
+                question: "Qui a entraîné Obi-Wan Kenobi ?",
+                choix: ["Yoda", "Mace Windu", "Qui-Gon Jinn", "Anakin"],
+                bonne: 2
+            },
+            {
+                question: "Quel est le nom du droïde de Luke ?",
+                choix: ["R2-D2", "C-3PO", "BB-8", "K-2SO"],
+                bonne: 0
+            },
+            {
+                question: "Qui tue Palpatine ?",
+                choix: ["Luke", "Yoda", "Anakin", "Rey"],
+                bonne: 2
+            },
+            {
+                question: "Quel est le nom de famille de la princesse Leia ?",
+                choix: ["Skywalker", "Organa", "Solo", "Kenobi"],
+                bonne: 1
+            },
+            {
+                question: "qui est le père de Luke et Leia ?",
+                choix: ["Obi-Wan Kenobi", "Han Solo", "Anakin Skywalker", "Mace Windu"],
+                bonne: 2
+            },
+            {
+                question: "Qui est le mari de la princesse Leia ?",
+                choix: ["Luke Skywalker", "Han Solo", "Anakin Skywalker", "Obi-Wan Kenobi"],
+                bonne: 1
+            },
+            {
+                question: "Quel est le nom du copilote Wookiee du Faucon Millenium ?",
+                choix: ["Yoda", "Chewbacca", "Darth Maul", "Boba Fett"],
+                bonne: 1
+            },
+            {
+                question: "Qui est le créateur de Star Wars ?",
+                choix: ["George Lucas", "Steven Spielberg", "James Cameron", "Peter Jackson"],
+                bonne: 0
+            },
+            {
+                question: "Combien de films composent la saga principale de Star Wars ?",
+                choix: ["7", "8", "9", "10"],
+                bonne: 2
+            },
+            {
+                question: "Quel est le nom du chasseur stellaire piloté par Anakin Skywalker lors de la bataille de Naboo ?",
+                choix: ["N-1", "X-Wing", "TIE Fighter", "A-Wing"],
+                bonne: 0
+            },
+            {
+                question: "Quel est le nom du commandant suprême du premier ordre avant d'être trahi par Kylo Ren ?",
+                choix: ["Dark Vador", "Empereur Palpatine", "Snoke", "General Grievous"],
+                bonne: 2
+            },
+            {
+                question: "Quel est le nom du célèbre chasseur de primes portant une armure mandalorienne ?",
+                choix: ["Boba Fett", "Jango Fett", "Din Djarin", "Cad Bane"],
+                bonne: 0
+            },
+            {
+                question: "Quel est le nom du vaisseau de Boba Fett ?",
+                choix: ["Slave I", "Faucon Millenium", "Star Destroyer", "X-Wing"],
+                bonne: 0
+            },
+            {
+                question: "Sous quel forme est sortie le premier Star Wars en 1977 ?",
+                choix: ["Film", "Série TV", "Livre", "Bande dessinée"],
+                bonne: 0
+            }
+        ];
+
+        /* =========================
+           QUESTIONS EN
+        ========================== */
+        const questionsEN = [
+            {
+                question: "What is Darth Vader's real name?",
+                choix: ["Luke Skywalker", "Anakin Skywalker", "Obi-Wan Kenobi", "Han Solo"],
+                bonne: 1
+            },
+            {
+                question: "Which planet is destroyed by the Death Star?",
+                choix: ["Hoth", "Alderaan", "Tatooine", "Naboo"],
+                bonne: 1
+            },
+            {
+                question: "Who trained Obi-Wan Kenobi?",
+                choix: ["Yoda", "Mace Windu", "Qui-Gon Jinn", "Anakin"],
+                bonne: 2
+            },
+            {
+                question: "What is Luke's droid called?",
+                choix: ["R2-D2", "C-3PO", "BB-8", "K-2SO"],
+                bonne: 0
+            },
+            {
+                question: "Who kills Palpatine?",
+                choix: ["Luke", "Yoda", "Anakin", "Rey"],
+                bonne: 2
+            },
+            {
+                question: "What is Princess Leia's last name?",
+                choix: ["Skywalker", "Organa", "Solo", "Kenobi"],
+                bonne: 1
+            },
+            {
+                question: "Who is the father of Luke and Leia?",
+                choix: ["Obi-Wan Kenobi", "Han Solo", "Anakin Skywalker", "Mace Windu"],
+                bonne: 2
+            },
+            {
+                question: "Who is Princess Leia's husband?",
+                choix: ["Luke Skywalker", "Han Solo", "Anakin Skywalker", "Obi-Wan Kenobi"],
+                bonne: 1
+            },
+            {
+                question: "What is the name of the Wookiee co-pilot of the Millennium Falcon?",
+                choix: ["Yoda", "Chewbacca", "Darth Maul", "Boba Fett"],
+                bonne: 1
+            },
+            {
+                question: "Who is the creator of Star Wars?",
+                choix: ["George Lucas", "Steven Spielberg", "James Cameron", "Peter Jackson"],
+                bonne: 0
+            },
+            {
+                question: "How many films make up the main Star Wars saga?",
+                choix: ["7", "8", "9", "10"],
+                bonne: 2
+            },
+            {
+                question: "What is the name of the starfighter piloted by Anakin Skywalker during the Battle of Naboo?",
+                choix: ["N-1", "X-Wing", "TIE Fighter", "A-Wing"],
+                bonne: 0
+            },
+            {
+                question: "What is the name of the Supreme Commander of the First Order before being betrayed by Kylo Ren?",
+                choix: ["Darth Vader", "Emperor Palpatine", "Snoke", "General Grievous"],
+                bonne: 2
+            },
+            {
+                question: "What is the name of the famous bounty hunter wearing Mandalorian armor?",
+                choix: ["Boba Fett", "Jango Fett", "Din Djarin", "Cad Bane"],
+                bonne: 0
+            },
+            {
+                question: "What is the name of Boba Fett's ship?",
+                choix: ["Slave I", "Millennium Falcon", "Star Destroyer", "X-Wing"],
+                bonne: 0
+            },
+            {
+                question: "In what form was the first Star Wars released in 1977?",
+                choix: ["Movie", "TV Series", "Book", "Comic Book"],
+                bonne: 0
+            }
+        ];
+
+        /* =========================
+           VARIABLES
+        ========================== */
+        let questions = questionsFR;
+        let index = 0;
+        let score = 0;
+        let debut;
+        let timerInterval;
+
+        /* =========================
+           TIMER
+        ========================== */
+        function lancerTimer() {
+            debut = Date.now();
+            clearInterval(timerInterval);
+
+            timerInterval = setInterval(() => {
+                let temps = Math.floor((Date.now() - debut) / 1000);
+                document.getElementById("timer").textContent = "Temps : " + temps + " s";
+            }, 1000);
+        }
+
+        /* =========================
+           AFFICHER QUESTION
+        ========================== */
+        function afficherQuestion() {
+            let q = questions[index];
+            document.getElementById("question").textContent = q.question;
+
+            let html = "";
+            q.choix.forEach((choix, i) => {
+                html += `
+                    <input type="radio" name="reponse" value="${i}">
+                    ${choix}<br>
+                `;
+            });
+
+            document.getElementById("reponses").innerHTML = html;
+        }
+
+        /* =========================
+           QUESTION SUIVANTE
+        ========================== */
+        function suivant() {
+            let selection = document.querySelector('input[name="reponse"]:checked');
+
+            if (!selection) {
+                alert("Choisis une réponse !");
+                return;
+            }
+
+            if (parseInt(selection.value) === questions[index].bonne) {
+                score++;
+            }
+
+            index++;
+
+            if (index < questions.length) {
+                afficherQuestion();
+            } else {
+                finQuiz();
+            }
+        }
+
+        /* =========================
+           FIN DU QUIZ
+        ========================== */
+        function finQuiz() {
+            clearInterval(timerInterval);
+            let tempsFinal = Math.floor((Date.now() - debut) / 1000);
+
+            document.getElementById("quiz").style.display = "none";
+            document.getElementById("resultat").innerHTML =
+                `🔥 Score : ${score} / ${questions.length}<br>
+                 ⏱ Temps : ${tempsFinal} secondes`;
+        }
+
+        /* =========================
+           LANGUE - ACCUEIL
+        ========================== */
+        function changerLangueAccueil(langue) {
+            if (langue === "fr") {
+                document.getElementById("titre").textContent = "Test le quiz pour voir si t'es un vrai fan de star wars.";
+                document.getElementById("soustitre").textContent = "Attention il y a un timer qui commence dès que tu appuies sur ce bouton.";
+                document.getElementById("btntext").textContent = "commencer le quiz";
+            } if (langue === "en") {
+                document.getElementById("titre").textContent = "Test the quiz to see if you are a true Star Wars fan.";
+                document.getElementById("soustitre").textContent = "Warning there is a timer that starts as soon as you press this button.";
+                document.getElementById("btntext").textContent = "start the quiz";
+            }
+        }
+
+        /* =========================
+           LANGUE - QUIZ
+        ========================== */
+        function changerLangue(langue) {
+            questions = langue === "fr" ? [...questionsFR] : [...questionsEN];
+            melangerQuestions(questions);
+            index = 0;
+            score = 0;
+
+            document.getElementById("quiz").style.display = "block";
+            document.getElementById("resultat").textContent = "";
+
+            lancerTimer();
+            afficherQuestion();
+        }
+
+        function melangerQuestions(tableau) {
+            for (let i = tableau.length - 1; i > 0; i--) {
+                let j = Math.floor(Math.random() * (i + 1));
+                [tableau[i], tableau[j]] = [tableau[j], tableau[i]];
+            }
+        }
+
+        /* =========================
+           LANCEMENT INITIAL
+        ========================== */
+        changerLangue("fr");
